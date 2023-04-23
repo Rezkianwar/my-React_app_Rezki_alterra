d1e4609f06e65fe5033bb86ef4b5b50e03360f50
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-04-23 15:12:30 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RouteManagement from "../router/RouteManagement";
+
+const App = () => {
+  return (
+    <>
+      <RouteManagement />
+    </>
+  );
+};
+
+export default App;
