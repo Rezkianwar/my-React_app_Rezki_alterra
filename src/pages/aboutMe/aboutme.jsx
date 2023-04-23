6a586de79d16d044844d7d24c27da3f291b03c19
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-04-23 15:13:41 +0700

    Create aboutme.jsx

---
+++ src/pages/aboutMe/aboutme.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { USERS_DATA } from "./Constants";
+import "./aboutMe.css";
+import { useParams } from "react-router-dom";
+import { useGetUsers } from "./Hooks/useUsersData";
+import { useEffect } from "react";
+import LoadingComponent from "../../Component/loadingComponent/loadingComponent";
+import { Card } from "antd";
+
+const Aboutme = () => {
+  const { id } = useParams();
+
+  const [isLoadingUserData, usersData, getUsersData] = useGetUsers();
+  console.log({ usersData });
+
+  const data = id ? USERS_DATA.filter((item) => item.id == id) : USERS_DATA;
+
+  useEffect(() => {
+    getUsersData();
+  }, []);
+  return (
+    <div>
+      <h1 className="title">About me</h1>
+      {isLoadingUserData ? (
+        <LoadingComponent />
+      ) : (
+        usersData?.map((user) => (
+          <Card title={user.firstName + " " + user.lastName} key={user.id}>
+            <div>{user.hobby}</div>
+            <div>{user.age}</div>
+          </Card>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Aboutme;
