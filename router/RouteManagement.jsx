f56e82a98fffd91d37fb2f7caa044145d32a3427
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-04-23 15:42:19 +0700

    Update RouteManagement.jsx

--- router/RouteManagement.jsx
+++ router/RouteManagement.jsx
@@ -2,7 +2,7 @@ import React, { Suspense, useEffect } from "react";
 import LoadingComponent from "../src/Component/loadingComponent/loadingComponent";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import FormComponent from "../src/pages/Form/FormComponent";
-import HomePage from "../src/pages/homepage/homepage";
+import HomePage from "../src/pages/LandingPage/homepage/homepage";
 import ProjectCalculator from "../src/pages/project-calculator/calculator";
 import Aboutme from "../src/pages/aboutMe/aboutme";
 import Formexp from "../src/pages/Form/Formexp";
